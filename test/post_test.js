cc1db6134c0647cce673713c32b44c31b71704cb
Dana Gajewska <mdgajewska@outlook.com>
2019-01-09 14:31:35 +0000

    Created #list, tests pass & truncate database after test

--- test/post_test.js
+++ test/post_test.js
@@ -4,6 +4,12 @@ const DatabaseConnection = require("../lib/database_connection")
 
 describe('Post', function() {
 
+  afterEach('Truncating dev database', function (){
+    const database = new DatabaseConnection("acebook_dev")
+    database.query('TRUNCATE posts RESTART IDENTITY CASCADE;')
+    database.query('TRUNCATE users RESTART IDENTITY CASCADE;')
+  })
+
   // describe('#create', function() {
   //   it("adds a post to the post database", function() {
   //
@@ -20,29 +26,32 @@ describe('Post', function() {
   //   })
   // })
 
-  describe('#view', function() {
+  describe('#list', function() {
     it("creates an array of all post objects", async function() {
 
       const databaseConnection = new DatabaseConnection("acebook_dev")
 
-      const userId = await databaseConnection.query(
+      let userId = await databaseConnection.query(
         "INSERT INTO users (firstname, lastname) " +
         "VALUES ('Ben', 'Johnson') " +
         "RETURNING userid;"
       )
+
+      userId = JSON.stringify(userId.rows[0]['userid']);
 
       await databaseConnection.query(
         "INSERT INTO posts (content, userid) " +
         `VALUES ('Heyy', '${userId}'), ('Life is good yo', '${userId}');`
       )
 
-      const posts = await Post.view();
+      const posts = await Post.list();
 
-      console.log(posts);
-
-      expect(posts[0].content).equal("Heyy")
-      expect(posts[1].content).equal("Life is good yo")
-
+      expect(posts.rows[0].content).equal("Heyy")
+      expect(posts.rows[0].firstname).equal("Ben")
+      expect(posts.rows[0].lastname).equal("Johnson")
+      expect(posts.rows[1].content).equal("Life is good yo")
+      expect(posts.rows[1].firstname).equal("Ben")
+      expect(posts.rows[1].lastname).equal("Johnson")
     })
   })
 
