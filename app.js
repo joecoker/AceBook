414347587074155975a200eff963f6018257cbe3
Stuart Adair <stu@silverka.co.uk>
2019-01-09 11:03:36 +0000

    Set up database_connection.js

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require('express');
+require('dotenv').config();
 const PORT = 3000;
 const app = express();
 const bodyParser = require('body-parser')
 const Post = require('./lib/post')
+const DatabaseConnection = require('./lib/database_connection')
+
+dbc = new DatabaseConnection();
+
+async function test() {
+    console.log(dbc);
+    testvalue = await dbc.query('SELECT * FROM users');
+    console.log(testvalue.rows);
+   }
+
+test();
 
 app.use(express.static('public'))
 
@@ -16,6 +28,10 @@ app.post('/newsfeed', async function(req, res) {
   const userId = req.body.userId;
   await Post.create(postContent, userId);
   res.redirect('/newsfeed')
+})
+
+app.get('/test', function(req, res) {
+
 })
 
 app.listen(PORT);
