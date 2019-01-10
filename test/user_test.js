84f2371afb0c0ca4f21697970e9e1594528fd56c
joecoker <joe_coker@hotmail.co.uk>
2019-01-10 14:42:05 +0000

    Add signin test and changes to database connection

--- test/user_test.js
+++ test/user_test.js
@@ -1,68 +1,52 @@
 const expect = require('chai').expect;
 const User = require('../lib/user');
-const DatabaseConnection = require('../lib/database_connection');
-const dbc = new DatabaseConnection();
 const DatabaseHelpers = require('./database_helpers')
 
 describe('User', function() {
 
- afterEach('Truncating dev database', function (){
-  const database = new DatabaseConnection("acebook_dev")
-  database.query('TRUNCATE posts RESTART IDENTITY CASCADE;')
-  database.query('TRUNCATE users RESTART IDENTITY CASCADE;')
- })
-
- describe('#create', function() {
-  it("creates a new user", async function() {
-
-   var user = await User.create('Test', 'Person', 'test@test.com', 'qwerty', '1993-04-23');
-
-   const database = new DatabaseConnection("acebook_dev")
-   user = await database.query('SELECT * FROM users;')
-
-   expect(user.rows[0].firstname).equal("Test");
-   expect(user.rows[0].lastname).equal("Person");
-   expect(user.rows[0].email).equal("test@test.com");
-   // expect(user.rows[0].password).equal("qwerty");
-
+  afterEach('Truncating dev database', async function (){
+    await DatabaseHelpers.truncateDatabase();
   })
 
-  it("cannot create an existing user", async function() {
+  describe('#create', function() {
+    it("creates a new user", async function() {
 
-    var user = await User.create('Test', 'Person', 'test@test.com', 'qwerty', '1993-04-23');
-    var user = await User.create('Test', 'Person', 'test@test.com', 'qwerty', '1993-04-23');
+      var user = await User.create('Test', 'Person', 'test@test.com', 'qwerty', '1993-04-23', '', 'acebook_dev');
+      let user = await database.query('SELECT * FROM users;');
 
-    expect(user).equal(false);
+      expect(user.rows[0].firstname).equal("Test");
+      expect(user.rows[0].lastname).equal("Person");
+      expect(user.rows[0].email).equal("test@test.com");
+    })
 
-  })
- })
+    it("cannot create an existing user", async function() {
 
- describe('#checkUserExists', function() {
-  it('returns true if user exists', async function() {
+      var user = await User.create('Test', 'Person', 'test@test.com', 'qwerty', '1993-04-23', '', 'acebook_dev');
+      var user = await User.create('Test', 'Person', 'test@test.com', 'qwerty', '1993-04-23', '', 'acebook_dev');
 
-   var user = await User.create('Test', 'Person', 'test@test.com', 'qwerty', '1993-04-23');
-
-   // const database = new DatabaseConnection("acebook_dev")
-
-   var exists = await User.checkUserExists('test@test.com')
-
-   expect(exists).equal(true);
+      expect(user).equal(false);
+    })
   })
 
-  it('returns false if user does not exist', async function() {
+  describe('#checkUserExists', function() {
+    it('returns true if user exists', async function() {
 
-   var user = await User.create('Test', 'Person', 'test@test.com', 'qwerty', '1993-04-23');
+      var user = await User.create('Test', 'Person', 'test@test.com', 'qwerty', '1993-04-23');
+      var exists = await User.checkUserExists('test@test.com');
 
-   // const database = new DatabaseConnection("acebook_dev")
+      expect(exists).equal(true);
+    })
 
-   var exists = await User.checkUserExists('not_a_user@test.com')
+    it('returns false if user does not exist', async function() {
 
-   expect(exists).equal(false);
+      var user = await User.create('Test', 'Person', 'test@test.com', 'qwerty', '1993-04-23');
+      var exists = await User.checkUserExists('not_a_user@test.com');
+
+      expect(exists).equal(false);
+    })
   })
 
- })
-
- describe('#signIn', function() {
+  describe('#signIn', function() {
    it('returns a users details if they exist', async function() {
 
     let userId = await DatabaseHelpers.createUser();
@@ -75,6 +59,6 @@ describe('User', function() {
     expect(user.rows[0].dob).equal("1993-04-23");
 
    })
- })
+  })
 
 })
