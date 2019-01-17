e691b0e557b3d83adc21209be40faaa95886d8a1
alittlecross <alittlecross@live.co.uk>
2019-01-17 13:02:34 +0000

    minor amendments made to a number of files during code-review

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const Comment = require('./lib/comment');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-let userId = 0;
 const sessionStore = new session.MemoryStore;
 
 const DATE_RFC2822 = "ddd, DD MMM YYYY HH:mm:ss ZZ";
@@ -60,7 +59,7 @@ app.post('/sign-up', async function(req, res) {
   }
 })
 
-app.get('/sign-in', function(req, res) {
+app.get('/sign-in', function(_req, res) {
   res.render('sign-in.ejs', { sessionFlash: res.locals.sessionFlash });
 })
 
@@ -73,7 +72,6 @@ app.post('/sign-in', async function(req, res) {
     }
     res.redirect('/sign-in')
   } else if (userDetails === false) {
-    // Flash message saying password is incorrect
     req.session.sessionFlash = {
       type: 'sign_in',
       message: 'Password incorrect.'
@@ -92,12 +90,19 @@ app.get('/newsfeed', async function(req, res) {
   }
   let posts = await Post.list(req.session.userId);
   let userDetails = await User.getProfile(req.session.userId);
-  res.render('newsfeed.ejs', {posts: posts, moment: moment, dateLongFormat: DATE_RFC2822, dateShortFormat: DATE_SHORT, userDetails: userDetails, loggedInUser: req.session.userId});
+  res.render('newsfeed.ejs', {
+    posts: posts, 
+    moment: moment, 
+    dateLongFormat: DATE_RFC2822, 
+    dateShortFormat: DATE_SHORT, 
+    userDetails: userDetails, 
+    loggedInUser: req.session.userId
+  });
 })
 
 app.post('/newsfeed', async function(req, res) {
   let postContent = req.body.postContent;
-  let result = await Post.create(postContent, req.session.userId);
+  await Post.create(postContent, req.session.userId);
   res.redirect('/newsfeed')
 })
 
@@ -105,7 +110,13 @@ app.get('/post/:postid', async function(req, res){
   let postId = req.params.postid;
   let post = await Post.getPost(postId, req.session.userId);
   let comments = await Comment.list(postId);
-  res.render('post.ejs', {post: post, comments: comments, commentCount: comments.length, moment: moment, dateLongFormat: DATE_RFC2822});
+  res.render('post.ejs', {
+    post: post, 
+    comments: comments, 
+    commentCount: comments.length, 
+    moment: moment, 
+    dateLongFormat: DATE_RFC2822
+  });
 })
 
 app.get('/post/like/:postid', async function(req, res){
@@ -115,14 +126,14 @@ app.get('/post/like/:postid', async function(req, res){
     return;
   }
   let postId = req.params.postid;
-  let likeId = await Like.toggleLike(postId, req.session.userId);
+  await Like.toggleLike(postId, req.session.userId);
   res.redirect(referrer);
 })
 
 app.post('/comment/:postid', async function(req, res){
   let postId = req.params.postid;
   let commentContent = req.body.commentContent;
-  let result = await Comment.create(commentContent, postId, req.session.userId);
+  await Comment.create(commentContent, postId, req.session.userId);
   res.redirect('/post/' + postId);
 })
 
@@ -130,7 +141,14 @@ app.get('/user/:userid', async function(req, res){
   let userIdProfile = req.params.userid;
   let userDetails = await User.getProfile(userIdProfile);
   let posts = await Post.getUserPosts(userIdProfile);
-  res.render('profile.ejs', {userDetails: userDetails, posts: posts, moment: moment, dateLongFormat: DATE_RFC2822, dateShortFormat: DATE_SHORT, loggedInUser: req.session.userId});
+  res.render('profile.ejs', {
+    userDetails: userDetails, 
+    posts: posts, 
+    moment: moment, 
+    dateLongFormat: DATE_RFC2822, 
+    dateShortFormat: DATE_SHORT, 
+    loggedInUser: req.session.userId
+  });
 })
 
 app.get('/user/edit/:userid', async function(req, res){
@@ -139,7 +157,14 @@ app.get('/user/edit/:userid', async function(req, res){
 })
 
 app.post('/user/update/:userid', async function(req, res){
-  let userDetails = await User.updateProfile(req.params.userid, req.body.firstName, req.body.lastName, req.body.email, req.body.dob, req.body.profilepictureurl);
+  let userDetails = await User.updateProfile(
+    req.params.userid, 
+    req.body.firstName, 
+    req.body.lastName, 
+    req.body.email, 
+    req.body.dob, 
+    req.body.profilepictureurl
+  );
   res.redirect('/user/' + userDetails.userid);
 })
 
@@ -149,6 +174,6 @@ app.get('/logout', async function(req, res){
 })
 
 app.listen(PORT);
-console.log(`Node listening on ${PORT}`);
+console.log(`Acebook listening on ${PORT}`);
 
 module.exports = app;
